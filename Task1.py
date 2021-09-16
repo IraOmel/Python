edee37164d187af51c6cfdb338ae356cf847ad39
Ira Omelchenko <omelira0@gmail.com>
2021-09-16 13:24:34 +0300

    New

--- Task1.py
+++ Task1.py
@@ -5,11 +5,13 @@ parameters = sys.argv[1:]  # get parameters from command line
 num1 = str(parameters[0])
 sign = str(parameters[1])
 num2 = str(parameters[2])
-try:
-    command = num1 + sign + num2
-    result = eval(command)
-    print(result)
-except ZeroDivisionError:
+if num1.isdigit() and num2.isdigit():
+    try:
+        command = num1 + sign + num2
+        result = eval(command)
+        print(result)
+    except ZeroDivisionError:
+        print("Error")
+else:
     print("Error")
-
-
+    quit()
--- Task2.py
+++ Task2.py
@@ -8,7 +8,7 @@ num2 = str(parameters[2])
 sign = {'add': '+', 'sub': '-', 'mul': '*', 'div': '/'}  # dictionary with operations
 if operation not in sign:
     print("Unknown operation")
-    exit(1)
+    quit()
 try:
     command = num1 + sign[operation] + num2
     result = eval(command)  # get result of operation
